bc50b017cfdaf643f3ea6d4165ec468a09b51467
Suyog3g <113542889+Suyog3g@users.noreply.github.com>
2023-11-20 06:06:21 -0500

    added fetch api
    added fetch api

---
+++ js/main.js
@@ -0,0 +1,81 @@
+(() => {
+  const model = document.querySelector("#model");
+  const hotspots = document.querySelectorAll(".Hotspot");
+
+  const materialTemplate = document.querySelector("#material-template");
+  const materialList = document.querySelector("#material-list");
+
+  function modelLoaded() {
+    hotspots.forEach(hotspot => {
+      hotspot.style.display = "block";
+    });
+  }
+
+  function loadInfoBoxes() {
+    fetch("https://swiftpixel.com/earbud/api/infoboxes")
+      .then(response => response.json())
+      .then(infobox => {
+        infobox.forEach((infoBox, index) => {
+          let selected = document.querySelector(`#hotspot-${index + 1}`);
+
+          // Create an image element
+          const imgElement = document.createElement('img');
+          imgElement.src = `images/${infoBox.thumbnail}`;
+          imgElement.alt = infoBox.heading; // Set alt attribute for accessibility
+
+          const titleElement = document.createElement('h2');
+          titleElement.textContent = infoBox.heading;
+
+          const textElement = document.createElement('p');
+          textElement.textContent = infoBox.description;
+
+          selected.appendChild(imgElement);
+          selected.appendChild(titleElement);
+          selected.appendChild(textElement);
+        });
+      })
+      .catch(failure => console.error(failure));
+  }
+
+  function loadMaterialInfo() {
+    fetch("https://swiftpixel.com/earbud/api/materials")
+      .then(response => response.json())
+      .then(materials => {
+        materials.forEach(materialPart => {
+          const clone = materialTemplate.content.cloneNode(true);
+
+          const materialHeading = clone.querySelector(".material-heading");
+          materialHeading.textContent = materialPart.heading;
+
+          const materialDesc = clone.querySelector(".material-description");
+          materialDesc.textContent = materialPart.description;
+
+          materialList.appendChild(clone);
+        });
+
+        partsInfo.innerHTML = "";
+        partsInfo.appendChild(materialList);
+      })
+      .catch(error => console.error(error));
+  }
+
+  loadMaterialInfo();
+  loadInfoBoxes();
+
+  function showInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 1 });
+  }
+
+  function hideInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 0 });
+  }
+
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+    hotspot.addEventListener("mouseenter", showInfo);
+    hotspot.addEventListener("mouseleave", hideInfo);
+  });
+})();
