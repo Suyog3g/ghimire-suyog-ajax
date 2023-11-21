958913d1390779a6118bada5b88044d60463c7eb
Suyog3g <113542889+Suyog3g@users.noreply.github.com>
2023-11-20 23:42:37 -0500

    final debugging
    final debugging

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 (() => {
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
-
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
-  const partsInfo = document.querySelector("#parts-info")
+  const materialInfo = document.querySelector("#material-info");  
 
   let spinner = '<svg width="512" height="512" viewBox="0 0 512 512" style="color:currentColor" xmlns="http://www.w3.org/2000/svg" class="h-full w-full"><rect width="512" height="512" x="0" y="0" rx="30" fill="transparent" stroke="transparent" stroke-width="0" stroke-opacity="100%" paint-order="stroke"></rect><svg width="256px" height="256px" viewBox="0 0 24 24" fill="currentColor" x="128" y="128" role="img" style="display:inline-block;vertical-align:middle" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><rect width="10" height="10" x="1" y="1" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle30" fill="freeze" attributeName="x" begin="0;svgSpinnersBlocksShuffle3b.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle31" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle38.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle32" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle39.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle33" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle3a.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="1" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle34" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle30.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle35" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle31.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle36" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle32.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle37" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle33.end" dur="0.2s" values="13;1"/></rect><rect width="10" height="10" x="13" y="13" fill="currentColor" rx="1"><animate id="svgSpinnersBlocksShuffle38" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle34.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle39" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle35.end" dur="0.2s" values="13;1"/><animate id="svgSpinnersBlocksShuffle3a" fill="freeze" attributeName="x" begin="svgSpinnersBlocksShuffle36.end" dur="0.2s" values="1;13"/><animate id="svgSpinnersBlocksShuffle3b" fill="freeze" attributeName="y" begin="svgSpinnersBlocksShuffle37.end" dur="0.2s" values="1;13"/></rect></g></svg></svg>'
 
@@ -21,7 +20,6 @@
         infobox.forEach((infoBox, index) => {
           let selected = document.querySelector(`#hotspot-${index + 1}`);
 
-          
           const imgElement = document.createElement('img');
           imgElement.src = `images/${infoBox.thumbnail}`;
 
@@ -36,11 +34,19 @@
           selected.appendChild(textElement);
         });
       })
-      .catch(failure => console.error(failure));
+      .catch(error => console.error('Oppssomethingiswrong', error));
   }
+ 
+  
+  function appendElement(parent, tag, attributes) {
+    const element = document.createElement(tag);
+    Object.entries(attributes).forEach(([key, value]) => (element[key] = value));
+    parent.appendChild(element);
+  }
+  
 
   function loadMaterialInfo() {
-    partsInfo.innerHTML = spinner;
+    materialInfo.innerHTML = spinner; 
     fetch("https://swiftpixel.com/earbud/api/materials")
       .then(response => response.json())
       .then(materials => {
@@ -56,10 +62,11 @@
           materialList.appendChild(clone);
         });
 
-        partsInfo.innerHTML = "";
-        partsInfo.appendChild(materialList);
+        materialInfo.innerHTML = ""; 
+        materialInfo.appendChild(materialList); 
       })
-      .catch(error => console.error(error));
+
+      .catch(error => console.error('Oppssomethingiswrong', error));
   }
 
   loadMaterialInfo();
